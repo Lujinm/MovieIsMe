45fa90ec8a45fed323cce8af6ef798145572c6ef
lujin <lujinmohammed223@gmail.com>
2025-01-19 16:42:05 +0300

    Initial Commit

---
+++ MovieIsMe/MovieIsMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieIsMeApp.swift
+//  MovieIsMe
+//
+//  Created by lujin mohammed on 19/07/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct MovieIsMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MovieIsMeTests/MovieIsMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieIsMeTests.swift
+//  MovieIsMeTests
+//
+//  Created by lujin mohammed on 19/07/1446 AH.
+//
+
+import Testing
+@testable import MovieIsMe
+
+struct MovieIsMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
